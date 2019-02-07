c1fdcb33bfa4cd34bf22f8378a7b45dab6b870b9
Nic Ferrier <nic@ferrier.me.uk>
2019-02-07 09:29:09 +0000

    better modularity

--- script.js
+++ script.js
@@ -9,37 +9,28 @@ function makeCard(suitSymbol, card) {
     }[suitSymbol];
     const cardTemplate = document.querySelector("#" + card);
     const cardDom = cardTemplate.content.cloneNode(true);
-    const symbols = Array.from(cardDom.firstElementChild.children);
+    const cardElement = cardDom.firstElementChild;
+    const symbols = Array.from(cardElement.children);
     symbols.forEach(symbol => symbol.innerHTML = "&" + suitSymbol + ";");
     const colorClass = red ? "red" : "black";
-    cardDom.firstElementChild.classList.add(colorClass);
-    return cardDom.firstElementChild;
+    cardElement.classList.add(colorClass);
+    cardElement.addEventListener("touchmove", evt => {
+        var touch = evt.targetTouches[0];
+        cardElement.style.position = "absolute";
+        const offsetWidth = touch.target.offsetWidth / 2;
+        const offsetHeight = touch.target.offsetHeight / 2;
+        cardElement.style.left = (touch.pageX - offsetWidth) + 'px';
+        cardElement.style.top = (touch.pageY - offsetHeight) + 'px';
+        evt.preventDefault();
+    });
+    return cardElement;
 }
 
 window.addEventListener("load", evt => {
     initCardTemplates();
     const section = document.querySelector("section.deck");
     const packTop = makeCard("spades", "back");
-    packTop.setAttribute("draggable", "true");
-
-    // not sure if I should drag things
-    packTop.addEventListener("dragstart", evt => {
-        var touch = evt.target;
-        packTop.style.position = "absolute";
-        packTop.style.left = touch.pageX - 50 + 'px';
-        packTop.style.top = touch.pageY - 50 + 'px';
-        evt.preventDefault();
-    });
-
-    packTop.addEventListener("touchmove", evt => {
-        var touch = evt.targetTouches[0];
-        packTop.style.position = "absolute";
-        const offsetWidth = touch.target.offsetWidth / 2;
-        const offsetHeight = touch.target.offsetHeight / 2;
-        packTop.style.left = (touch.pageX - offsetWidth) + 'px';
-        packTop.style.top = (touch.pageY - offsetHeight) + 'px';
-        evt.preventDefault();
-    });
+    const pack = [];
     section.appendChild(packTop);
 });
 
